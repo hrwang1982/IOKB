1a93d2632c5995909ab13098540944193b290be0
hrwang1982 <190143683@qq.com>
2026-01-28 00:06:30 +0800

    问题优化： InfluxDB 报错 401 Unauthorized 是因为 Docker 容器初始化时生成的随机 Token 与后端应用配置的 Token 不一致。
    修复操作：
    
    修改了 docker-compose.yml，显式设置 DOCKER_INFLUXDB_INIT_ADMIN_TOKEN=skb-influxdb-token。
    更新了 .env 文件，将 INFLUXDB_TOKEN 设置为相同的值。
    重建了 InfluxDB 容器并重置了其数据卷，以确保新 Token 生效。

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4b7b358a098cb4a5")
+/******/ 		__webpack_require__.h = () => ("a750e5e7b04cad1a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
